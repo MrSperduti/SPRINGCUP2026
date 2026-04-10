3219012130d1e5f6cffaf9e1f89fc9043d469882
MrSperduti <gianluca.sperduti@sctech.info>
2026-04-10 16:38:52 +0200

    Update categoria.js

--- categoria.js
+++ categoria.js
@@ -8,11 +8,13 @@ async function loadCategoria() {
   titolo.textContent = categoria;
 
   let html = `
-    <a class="btn" href="gironi.html?categoria=${categoria}">📊 Gironi</a>
     <a class="btn" href="calendario.html?categoria=${categoria}">📅 Calendario</a>
-    <a class="btn" href="partite.html?categoria=${categoria}">⚽ Partite</a>
-    <a class="btn" href="classifica.html?categoria=${categoria}">🏆 Classifica</a>
-    <a class="btn" href="rose.html?categoria=${categoria}">👥 Rose</a>
+  <a class="btn" href="classifica.html?categoria=${categoria}">🏆 Classifica</a>
+  <a class="btn" href="gironi.html?categoria=${categoria}">📊 Gironi</a>
+  <a class="btn" href="rose.html?categoria=${categoria}">👥 Rose</a>
+  <a class="btn" href="classifica-marcatori.html?categoria=${categoria}">⚽ Marcatori</a>
+  <a class="btn" href="miglior-giocatore.html?categoria=${categoria}">⭐ Miglior Giocatore</a>
+  <a class="btn" href="miglior-portiere.html?categoria=${categoria}">🧤 Miglior Portiere</a>
   `;
 
   menu.innerHTML = html;
